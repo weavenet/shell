9e79b37b8a97d9b8f852fa8f7d87d91b61eae13d
Brett W <brett9000@icloud.com>
2019-09-20 12:47:55 -0700

    fix mount path

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,6 @@ image=$1
 
 echo "Starting container."
 
-docker run -d -p 22:22 -v ~/code:/home/ec2-user/code $image
+docker run -d -p 22:22 -v ~/code:/home/user/code $image
 
 echo "Container succesfully started."
